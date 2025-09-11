ff385cf328b1365746f66cfb90b2d6784293b57a
neto_desktop <idervaljose00@gmail.com>
2025-09-11 16:52:50 -0300

    alteração

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\n\nHello, World!");
-Console.WriteLine("\nPRIMEIRO COMANDOOOO !!!\n\n");
+﻿using DotNet.Models;
+
+Pessoa p = new Pessoa();
+p.Nome = "Neto";
+p.Idade = 23;
+
+p.Apresentar();
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine($"E são:  {dataAtual.ToString("HH")} Horas e {dataAtual.ToString("mm")} Minutos");
